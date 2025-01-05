66348ac0860e896ba3c5aedc54a1dc120cf7c683
SUGITH0306 <its.me.sugith@gmail.com>
2025-01-05 15:14:15 +0530

    Create Length of the line.java

---
+++ Length of the line.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt(), y1 = sc.nextInt();
+        int x2 = sc.nextInt(), y2 = sc.nextInt();
+        
+        double d = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        
+        System.out.printf("%.2f\n", d);
+    }
+}
