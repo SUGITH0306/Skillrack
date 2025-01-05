acc83c8f02542d264f9de2fe5e986cc1ede85f5a
SUGITH0306 <its.me.sugith@gmail.com>
2025-01-05 14:20:39 +0530

    Create Reverse and remove letters in vowel positions.java

---
+++ Reverse and remove letters in vowel positions.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] a) {
+        Scanner s = new Scanner(System.in);
+        String in = s.nextLine().trim(), v = "aeiouAEIOU";
+        Set<Integer> p = new HashSet<>();
+        for (int i = 0; i < in.length(); i++) if (v.indexOf(in.charAt(i)) != -1) p.add(i);
+        StringBuilder r = new StringBuilder(in).reverse(), res = new StringBuilder();
+        for (int i = 0; i < r.length(); i++) if (!p.contains(i)) res.append(r.charAt(i));
+        System.out.println(res);
+    }
+}
