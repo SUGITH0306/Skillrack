d5831e9236e20aba990eb19eb08bf5ed6bf8a89f
Sugith Kanna <its.me.sugith@gmail.com>
2025-01-07 19:35:49 +0530

    Shift Encryption.java

--- Shift Encryption.java
+++ Shift Encryption.java
@@ -1,18 +1,16 @@
-import java.util.*;
-public class Main {
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] a) {
         Scanner s = new Scanner(System.in);
-        String m = s.nextLine(); 
-        int x = s.nextInt(), y = s.nextInt(); 
-        StringBuilder e = new StringBuilder();  
+        String m = s.nextLine();
+        int x = s.nextInt(), y = s.nextInt();
+        StringBuilder e = new StringBuilder();
         for (char c : m.toCharArray()) {
-            if (Character.isLowerCase(c)) 
-                e.append((char) (((c - 'a' + x) % 26) + 'a'));
-            else if (Character.isDigit(c)) 
-                e.append((char) (c + y));
-            else 
-                e.append(c);
+            if (Character.isLowerCase(c)) e.append((char)((c - 'a' + x) % 26 + 'a'));
+            else if (Character.isDigit(c)) e.append((char)((c - '0' + y) % 10 + '0'));
+            else e.append(c);
         }
-        System.out.println(e.toString());
+        System.out.println(e);
     }
 }
