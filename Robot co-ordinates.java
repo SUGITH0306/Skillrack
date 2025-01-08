f08354e62652d16ed97bb21fee2ac8969826fb85
Sugith Kanna <its.me.sugith@gmail.com>
2025-01-09 01:01:11 +0530

    Create Robot co-ordinates.java

---
+++ Robot co-ordinates.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] a) {
+        Scanner s = new Scanner(System.in);
+        String[] p = s.nextLine().split(" ");
+        int x = Integer.parseInt(p[0].substring(1)), y = Integer.parseInt(p[1].substring(1));
+        for (int i = 2; i < p.length; i++) {
+            int d = Integer.parseInt(p[i].substring(1));
+            if (p[i].charAt(0) == 'N') y += d;
+            else if (p[i].charAt(0) == 'E') x += d;
+            else if (p[i].charAt(0) == 'S') y -= d;
+            else x -= d;
+        }
+        System.out.println("x" + x + " y" + y);
+    }
+}
