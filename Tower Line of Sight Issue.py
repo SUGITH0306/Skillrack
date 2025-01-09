01f26c275557dd531fb38e32457173c23f8b9045
Sugith Kanna <its.me.sugith@gmail.com>
2025-01-09 21:04:51 +0530

    Update Tower Line of Sight Issue.py

--- Tower Line of Sight Issue.py
+++ Tower Line of Sight Issue.py
@@ -6,3 +6,19 @@ f=f1=0
 if(((Bx<=Ax and By<=Cx)or(Bx>=Cx and Bx>=Ax))and((By<=Ay and By<=Cy)or(By>=Ay and By>=Cy))and((Dx<=Ax and Dx<=Cx)or(Dx>=Cx and Dx>=Ax))and((Dy<=Ay and Dy<=Cy)or(Dy>=Ay and Dy>=Cy))):f=1
 if(((Ax<=Bx and Ax<=Dx)or(Ax>=Bx and Ax>=Dx))and((Ay<=By and Ay<=Dy)or(Ay>=By and Ay>=Dy))and((Cx<=Bx and Cx<=Dx)or(Cx>=Bx and Cx>=Dx))and((Cy<=By and By<=Dy)or(Cy>=By and Cy>=Dy))):f1=1
 print("no" if f and f1 else "yes")
+
+Example Input/Output 1:
+0 0
+0 -2
+2 0
+0 2
+Output:
+yes
+Example Input/Output 2:
+Input:
+0 0
+0 -2
+2 0
+0 -5
+Output:
+no
