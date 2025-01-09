60db37e24f7b4139d2f6140591ee43c62f675c38
Sugith Kanna <its.me.sugith@gmail.com>
2025-01-10 01:12:37 +0530

    Update String - Count Articles.java

--- String - Count Articles.java
+++ String - Count Articles.java
@@ -8,3 +8,17 @@ public class Main {
         System.out.println(c);
     }
 }
+
+Example Input/Output 1:
+Input:
+I went to a movie yesterday along with an old man.
+Output:
+2
+Example Input/Output 2:
+Input
+In 1885 it was banished from the shelves of the Concord Public Library, an act that attracted a lot of publicity and discussion in
+the press. It is still frequently in the news, as various schools and school systems across the country either ban it from or restore
+it to their classrooms. The texts and illustrations below attempt to capture both the novel's achievement and some aspects of
+its controversiality.
+Output:
+8
