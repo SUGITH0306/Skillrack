0f4920d863369ddc22851aa614d9cbe30decf3e0
Sugith Kanna <its.me.sugith@gmail.com>
2025-01-12 21:11:56 +0530

    Update Array Product Except Index Value [AMAZON].java

--- Array Product Except Index Value [AMAZON].java
+++ Array Product Except Index Value [AMAZON].java
@@ -9,3 +9,19 @@ public class Main {
             System.out.print(p / Integer.parseInt(x[i++]) + (i < n ? " " : ""));
     }
 }
+
+Example Input/Output 1:
+Input:
+1 2 3 4 5
+Output:
+120 60 40 30 24
+Example Input/Output 2:
+Input:
+10 5 4
+Output:
+20 40 50
+Example Input/Output 3:
+Input:
+21 100
+Output:
+100 21
