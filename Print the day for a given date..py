d682706ccc3b78031cb6182742d186caa048113e
Sugith Kanna <its.me.sugith@gmail.com>
2025-01-13 01:55:45 +0530

    Update Print the day for a given date..py

--- Print the day for a given date..py
+++ Print the day for a given date..py
@@ -4,3 +4,16 @@ d = int(input())
 start_index = days.index(start_day)
 result_day = days[(start_index + (d - 1)) % 7]
 print(result_day)
+
+Example 1:
+Input
+MON
+10
+Output:
+WED
+Example 2:
+Input:
+FRI
+24
+Output:
+SUN
