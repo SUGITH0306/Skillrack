7774f0f1962c39f82fde2534dc56f75b26031511
Sugith Kanna <its.me.sugith@gmail.com>
2025-01-13 11:51:23 +0530

    Update Rotate Matrix 90 Degree Clockwise.java

--- Rotate Matrix 90 Degree Clockwise.java
+++ Rotate Matrix 90 Degree Clockwise.java
@@ -11,3 +11,27 @@ public class Main {
         }
     }
 }
+
+Example Input/Output 1:
+Input:
+2
+3
+4 5 9
+1 3 5
+Output:
+1 4
+3 5
+5 9
+Example Input/Output 2:
+Input
+4
+4
+1 2 3 4
+5 6 7 8
+9 10 11 12
+13 14 15 16
+Output:
+13 9 5 1
+14 10 6 2
+15 11 7 3
+16 12 8 4
